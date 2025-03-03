3b65d2b585fabec74d3a5b1407bee7dd3b1eaf78
DonMadden <maddendw@mail.uc.edu>
2025-03-02 23:11:32 -0500

    This one has task 1-4 for part A, added Nested loop for part B which covers task 5-7, added Die Roller for part C.

---
+++ src/DieRollar.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DieRollar {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+        boolean keepRolling = true;
+
+        while (keepRolling) {
+            int rollCount = 0;
+            int die1, die2, die3;
+
+            System.out.println("Roll\tDie1\tDie2\tDie3\tSum");
+            System.out.println("--------------------------------");
+
+            do {
+                rollCount++;
+                die1 = rand.nextInt(6) + 1;
+                die2 = rand.nextInt(6) + 1;
+                die3 = rand.nextInt(6) + 1;
+                int sum = die1 + die2 + die3;
+                System.out.printf("%d\t%d\t%d\t%d\t%d\n", rollCount, die1, die2, die3, sum);
+            } while (!(die1 == die2 && die2 == die3));
+
+            System.out.print("Roll again? (yes/no): ");
+            String response = scanner.next();
+            if (response.equalsIgnoreCase("no")) {
+                keepRolling = false;
+            }
+        }
+        scanner.close();
+    }
+}
