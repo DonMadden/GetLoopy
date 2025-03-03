9c9c08f3d66663754d66c688c33847b40b6c3bcf
DonMadden <maddendw@mail.uc.edu>
2025-03-02 23:08:30 -0500

    This one has task 1-4 for part A, added Nested loop for part B which covers task 5-7,

---
+++ src/NestedLoops.java
@@ -0,0 +1,32 @@
+public class NestedLoops {
+    public static void main(String[] args) {
+        // Task 5: Triangle Pattern
+        System.out.println("Task 5:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Task 6: Inverted Triangle Pattern
+        System.out.println("Task 6:");
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Task 7: Square Pattern
+        System.out.println("Task 7:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
